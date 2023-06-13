dc399f9eb788c93933cc951964eba9e981ac5991
Vlad Pugachev <valdis171186@gmail.com>
2023-06-13 20:22:26 +0500

    s

--- terraform/day_1/modules/ansible/k8s.tf
+++ terraform/day_1/modules/ansible/k8s.tf
@@ -1,7 +1,7 @@
 resource "null_resource" "deploy_k8s" {
   depends_on = [null_resource.bird_admin_bgw]
   provisioner "local-exec" {
-    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook --key-file ssh/id_rsa --ssh-extra-args='-o ProxyCommand=\"ssh -i ssh/id_rsa -o StrictHostKeyChecking=no -W %h:%p ${var.admin_bgw_external_ip}\"' -i ./modules/ansible/inventory.yml ./modules/ansible/kubespray/cluster.yml -b -v"
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook --key-file ssh/id_rsa --ssh-extra-args='-o ProxyCommand=\"ssh -i ssh/id_rsa -o StrictHostKeyChecking=no -W %h:%p ${var.admin_bgw_external_ip}\"' -i ./modules/ansible/inventory.yml ./modules/ansible/roles/kubespray/cluster.yml -b -v"
   }
 }
 
