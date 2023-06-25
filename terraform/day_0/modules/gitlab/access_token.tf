1925ce2ca1fc7ac857988a9895798084e87da38d
Pugachev Vlad <valdis171186@gmail.com>
2023-06-25 15:03:26 +0500

    fff

--- terraform/day_0/modules/gitlab/access_token.tf
+++ terraform/day_0/modules/gitlab/access_token.tf
@@ -2,4 +2,5 @@ resource "gitlab_project_access_token" "atlantis_api" {
   project      = gitlab_project.diplom_atlantis.id
   name         = "access token for day_1"
   scopes = ["api"]
+  expires_at = "2023-12-31"
 }
